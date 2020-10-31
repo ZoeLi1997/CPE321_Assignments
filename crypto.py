b4a03cdef32d1e947dedfe21ef290a25edc1b545
Ethan A <eahlquis@calpoly.edu>
2020-10-30 18:19:11 -0700

    second commit

--- crypto.py
+++ crypto.py
@@ -13,19 +13,19 @@ def xor(byte1, byte2):
     return bytes(a ^ b for a, b in zip(byte1, byte2))
 
 
-def ecb_encrypt(key, msg, IV=None):
-    if(IV):
-        msg = xor(msg, IV)
+def aes_encrypt(key, msg, XOR=None):
+    if(XOR):
+        msg = xor(msg, XOR)
     _AES = AES.new(key, AES.MODE_ECB)
     cipher = _AES.encrypt(msg)
     return cipher
 
 
-def ecb_decrypt(key, cipher, IV=None):
+def aes_decrypt(key, cipher, XOR=None):
     _AES = AES.new(key, AES.MODE_ECB)
     msg = _AES.decrypt(cipher)
-    if(IV):
-        msg = xor(msg, IV)
+    if(XOR):
+        msg = xor(msg, XOR)
     return msg
 
 
@@ -39,7 +39,7 @@ def ECB_encrypt(key, text):
     text = add_padding(text)
     encrypted = bytes()
     for i in range(0, len(text), C_SIZE):
-        e_chunk = ecb_encrypt(key, text[i:i+C_SIZE])
+        e_chunk = aes_encrypt(key, text[i:i+C_SIZE])
         encrypted += e_chunk
     return encrypted
 
@@ -47,7 +47,7 @@ def ECB_encrypt(key, text):
 def ECB_decrypt(key, cipher):
     decrypted = bytes()
     for i in range(0, len(cipher), C_SIZE):
-        d_chunk = ecb_decrypt(key, cipher[i:i+C_SIZE])
+        d_chunk = aes_decrypt(key, cipher[i:i+C_SIZE])
         decrypted += d_chunk
     return decrypted
 
@@ -59,7 +59,7 @@ def CBC_encrypt(key, text):
     e_chunk = IV
     encrypted = bytes(IV)
     for i in range(0, len(text), C_SIZE):
-        e_chunk = ecb_encrypt(key, text[i:i+C_SIZE], IV=e_chunk)
+        e_chunk = aes_encrypt(key, text[i:i+C_SIZE], XOR=e_chunk)
         encrypted += e_chunk
     return encrypted
 
@@ -68,7 +68,7 @@ def CBC_decrypt(key, cipher):
     decrypted = bytes()
     for i in range(C_SIZE, len(cipher), C_SIZE):
         prev_c_chunk = cipher[i-C_SIZE:i]
-        d_chunk = ecb_decrypt(key, cipher[i:i+C_SIZE], IV=prev_c_chunk)
+        d_chunk = aes_decrypt(key, cipher[i:i+C_SIZE], XOR=prev_c_chunk)
         if(i != 0):
             decrypted += d_chunk
     return decrypted
@@ -93,10 +93,10 @@ def task1():
     key = os.urandom(C_SIZE)
     cipher = CBC_encrypt(key, text)
     open(outfile + "CBC_enc.bmp", "wb").write(header +
-                                              cipher[C_SIZE:(len(text)+C_SIZE)])
+                                              cipher[C_SIZE:(len(text))])
     msg = CBC_decrypt(key, cipher)
     open(outfile + "CBC_dec.bmp", "wb").write(header +
-                                              msg[C_SIZE:(len(text)+C_SIZE)])
+                                              msg[C_SIZE:(len(text))])
 
 
 '''
