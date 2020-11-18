c6d4d890fbcbdc81526753175b1879946af25365
Zheng Zheng Li <zhengzhengli@zhengs-air.attlocal.net>
2020-11-18 14:41:34 -0800

    added more functions

--- asgn5/public.py
+++ asgn5/public.py
@@ -1,6 +1,6 @@
 from Crypto.Hash import SHA256 as sha
+from Crypto.Cipher import AES
 import random
-
 
 def get_pub_key(g, p):
     r = random.randint(1, p)
@@ -15,4 +15,16 @@ def get_key(s):
     h = sha.new()
     h.update(bin(s))
     return h.hexdigest()
+
+
+def encryption(msg, key):
+    _AES = AES.new(key, AES.MODE_CBC)
+    cipher = _AES.encrypt(msg)
+    return cipher
+
+
+def decryption(cipher, key):
+    _AES = AES.new(key, AES.MODE_CBC)
+    msg = _AES.decrypt(cipher)
+    return msg
 
